7588a30e00455c5dad4ba43ba2058d15e719003e
kwiatriot <wkwiatjr@me.com>
2021-01-01 16:12:55 -0700

    PEP8 compliance

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import hashlib
 import os
 import re
 
+
 # This function displays the menu that the user will encounter.
 def display_menu():
     print()
     print('Welcome to the Employee Management console. \n Please select from the following Items:')
-    print('1: Create new employee \n2: Manage login credentials \n3: Edit availability for employee \n4: Generate email list \n5: Quit')
+    print('1: Create new employee \n2: Manage login credentials \n3: Edit availability for employee \n'
+          '4: Generate email list \n5: Quit')
     print()
 
-#This function displays the password instructions.
+
+# This function displays the password instructions.
 def display_password_instructions():
     print()
     print('We need to set your password, please choose a password with the following:')
@@ -21,54 +24,57 @@ def display_password_instructions():
     print('Must have at least one special character from [ _ or @ or $ ].')
     print()
 
-#This function will cycle through the main menu 
+
+# This function will cycle through the main menu
 def make_menu_decision():
     while True:
         display_menu()
         choice = int(input("Please make your selection: "))
-        if choice == 1: #create new employee
+        if choice == 1:  # create new employee
             create_emp()
-        elif choice == 2: #manage login credentials
+        elif choice == 2:  # manage login credentials
             create_pass()
-        elif choice == 3: #edit availability
+        elif choice == 3:  # edit availability
             print("Edit availability for employee")
-        elif choice == 4: #generate email list
+        elif choice == 4:  # generate email list
             print("Generate email list")
         elif choice ==5:
             print("\nHave a nice day.\n")
             break
         else:
             print("I am sorry; I do not recognize this option")
-            
+
+
 # This function will add the first and last name to empData file. 
 # This will also create and store the email
 def create_emp():
     # TODO add logging info
     with open('empData.txt', 'a') as f:
-        addFirst = input("Enter the employee's first name: ")
-        addLast = input("Enter the employyee's last name: ")
-        newEmp = Employee(addFirst, addLast)
-        f.write(f'Name: {newEmp.full_name}, Email: {newEmp.email}\n')
+        add_first = input("Enter the employee's first name: ")
+        add_last = input("Enter the employee's last name: ")
+        new_emp = Employee(add_first, add_last)
+        f.write(f'Name: {new_emp.full_name}, Email: {new_emp.email}\n')
         print("Employee successfully added.")
+
 
 def check_emp_file():
     emp = input("Enter the employee's name: ")
 
     with open('empData.txt', 'r') as f:
         if emp in f.read():
-            return(True)
+            return True
         else:
             print("ERROR: No record of that name, please create employee first!")
 
 
-#Function to create a user password
+# Function to create a user password
 def create_pass():
     while check_emp_file() is True:
         display_password_instructions()
         password = input('Enter new password: ')
         errors = []  # Initialize this with zero elements. If an error occurs, then add it to array
-        if (len(password) < 8):
-                errors.append("Password length must be above 8 characters.")
+        if len(password) < 8:
+            errors.append("Password length must be above 8 characters.")
         if not re.search("[a-z]", password):
             errors.append("Password must contain at least one lowercase letter.")
         if not re.search("[A-Z]", password):
@@ -82,8 +88,8 @@ def create_pass():
         if re.search("\s", password):
             errors.append("No whitespace characters are allowed.")
 
-        if (len(errors) > 0):  # If errors contains ANY elements, then print the errors and loop through again.
-            print("The following error(s) occured.")
+        if len(errors) > 0:  # If errors contains ANY elements, then print the errors and loop through again.
+            print("The following error(s) occurred.")
             # Print each error using a new line to separate each error
             for error in errors:
                 print(error)
@@ -96,6 +102,7 @@ def create_pass():
                 f.write(f'Name: {user}\nSalt: {salt}\nKey: {key}')
             print("Valid password and stored")
             break
+
 
 # This is the entry point to the application. This is the first line of code that will be executed.
 make_menu_decision()
